4e48bc8e232a0c9ed6f67f65ef9a352f82ee720b
hwet-j <ghlckd5424@gmail.com>
2023-01-12 09:26:24 +0900

    #20 크롤링 재작업.. (git연동 오류-정보 날아감)

--- MatchList/test_code/crawl.py
+++ MatchList/test_code/crawl.py
@@ -8,20 +8,30 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-URL = 'https://pubg.op.gg/'
+def opggCrawling(name):
+    URL = 'https://pubg.op.gg/'
 
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get(url=URL)
-# 암시적 대기 (무조건 대기하지 않고 최대 5초)
-driver.implicitly_wait(time_to_wait=5)
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    # 브라우저 위치 설정
+    driver.set_window_position(0,0)
+    # 브라우저 크기 설정
+    driver.set_window_size(720, 800)
 
-# 아이디 검색
-input_id = 'Hwet_J'
-search_id = driver.find_element(By.XPATH, '//*[@id="searchPlayerText"]')
-search_id.send_keys(input_id)
-search_id.submit()            # 해당 위치에서 Enter 효과
-driver.implicitly_wait(time_to_wait=5)
+    driver.get(url=URL)
+    # 암시적 대기 (무조건 대기하지 않고 최대 5초)
+    driver.implicitly_wait(time_to_wait=5)
 
-# 플랫폼 확인
-platform = driver.find_element(By.XPATH, '/html/body/div[1]/section/div[2]/div/div/div/div/div[2]/div[1]/span').text
-platform_text = platform.find(r'([0-9A-Za-z]+)\_')
+    # 아이디 검색
+    input_id = name
+    search_id = driver.find_element(By.XPATH, '//*[@id="searchPlayerText"]')
+    search_id.send_keys(input_id)
+    search_id.submit()            # 해당 위치에서 Enter 효과
+    driver.implicitly_wait(time_to_wait=5)
+
+    # 플랫폼 확인
+    platform = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH , '/html/body/div[1]/section/div[2]/div/div/div/div/div[2]/div[1]/span')).text
+        )
+    platform_text = platform.find(r'([0-9A-Za-z]+)\_')
+
+
