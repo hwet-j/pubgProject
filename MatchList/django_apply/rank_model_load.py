80825d69499533d07d6601a3b8a6a90729f9f0e6
hwet-j <ghlckd5424@gmail.com>
2023-01-20 14:12:36 +0900

    #30 순위예측 ajax 완성

--- MatchList/django_apply/rank_model_load.py
+++ MatchList/django_apply/rank_model_load.py
@@ -22,27 +22,27 @@ def load_model(data):
        for i, j in zip(np.around(model_predict), rank_data):
               print(int(i), '\t' , j)
 
-def predict_one():
+def predict_one(data):
        import joblib   # 모델 저장, 불러오기
        import pandas as pd
        from sklearn.metrics import mean_absolute_error
        import numpy as np
-       from lightgbm import LGBMRegressor
+       row = pd.DataFrame()
+       row = row.append(data, ignore_index=True)
+       row = row[['dbnos','assists','boosts','damage_dealt','headshot_kills','heals','kills',
+                  'longest_kill','revives','total_distance','ride_distance','road_kills',
+                  'swim_distance','vehicle_destroys','time_survived','walk_distance',
+                  'weapons_acquired','win_place','team_count','map_name','duration']]
 
-       row = pd.read_csv("C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/crawl_test_data.csv")
        model = joblib.load("C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/lgbm_model.h5")
-
        # 맵 이름을 맵핑하기 위해 설정 (대회 기록으로 학습시킨 데이터를 경쟁전 데이터를 입력하므로 맵의 종류가 다름)
        map_ecoding = {'에란겔':0, '미라마':1, "태이고":2}
        row.replace({"map_name":map_ecoding}, inplace=True)
 
        test_data = row.drop(columns=['win_place'])
-       rank_data = row['win_place']
+       rank_data = row[['win_place']]
 
-       model_predict = model.predict(test_data)
-       print(np.around(model_predict))
-
-       print('mae : ', mean_absolute_error(np.around(model_predict), rank_data))
+       model_predict = int(round(model.predict(test_data)[0]))
        return model_predict
 
 
--- rank/views.py
+++ rank/views.py
@@ -36,102 +36,86 @@ def readData(request):
         name = request.POST['title']
         user_platform = request.POST['select']
 
-    path = "C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/" + name + ".csv"
-    all_path = "C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/db_save.csv"
-    if os.path.isfile(path):
-        # 파일이 존재하면 크롤링 작업을 하지 않도록 설정했지만 이후에 게임을 더 한다면
-        # 크롤링 작업이 필요하다.
-        # dakggCrawling(name, user_platform)
-        df_load = pd.read_csv(path)
-        df_all = pd.concat([df_all, df_load], ignore_index=True)
-        df_all.drop_duplicates(inplace=True)
-        df_all.to_csv(all_path, index=False)
-        time.sleep(0.5)
+    try:
+        path = "C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/" + name + ".csv"
+        all_path = "C:/Users/ghlck/PycharmProjects/pubg/MatchList/django_apply/datas/db_save.csv"
+        if os.path.isfile(path):
+            # 파일이 존재하면 크롤링 작업을 하지 않도록 설정했지만 이후에 게임을 더 한다면
+            # 크롤링 작업이 필요하다.
+            # dakggCrawling(name, user_platform)
+            df_load = pd.read_csv(path)
+            df_all = pd.concat([df_all, df_load], ignore_index=True)
+            df_all.drop_duplicates(inplace=True)
+            df_all.to_csv(all_path, index=False)
+            time.sleep(0.5)
 
-        file = open(all_path, encoding="UTF8")
-        reader = csv.reader(file)
-    else:
-        dakggCrawling(name, user_platform)
-        df_load = pd.read_csv(path)
-        df_all = pd.concat([df_all, df_load], ignore_index=True)
-        df_all.drop_duplicates(inplace=True)
-        df_all.to_csv(all_path, index=False)
-        time.sleep(0.5)
+            file = open(all_path, encoding="UTF8")
+            reader = csv.reader(file)
+        else:
+            dakggCrawling(name, user_platform)
+            df_load = pd.read_csv(path)
+            df_all = pd.concat([df_all, df_load], ignore_index=True)
+            df_all.drop_duplicates(inplace=True)
+            df_all.to_csv(all_path, index=False)
+            time.sleep(0.5)
 
-        file = open(all_path, encoding="UTF8")
-        reader = csv.reader(file)
+            file = open(all_path, encoding="UTF8")
+            reader = csv.reader(file)
 
-    PersonalData.objects.all().delete()
-    # print(reader)     # csv파일을 성공적으로 불러오는지 확인.
+        PersonalData.objects.all().delete()
+        # print(reader)     # csv파일을 성공적으로 불러오는지 확인.
 
-    list = []
-    for count, row in enumerate(reader):
-        if count == 0:
-            continue
-        list.append(PersonalData(
-        user_id =           row[0],
-        dbnos =             row[1],
-        assists =           row[2],
-        boosts =            row[3],
-        damage_dealt =      row[4],
-        headshot_kills =    row[5],
-        heals =             row[6],
-        kills =             row[7],
-        longest_kill =      row[8],
-        revives =           row[9],
-        total_distance =    row[10],
-        ride_distance =     row[11],
-        road_kills =        row[12],
-        swim_distance =     row[13],
-        vehicle_destroys =  row[14],
-        time_survived =     row[15],
-        walk_distance =     row[16],
-        weapons_acquired =  row[17],
-        team_count =        row[18],
-        map_name =          row[19],
-        duration =          row[20],
-        win_place =         row[21]
-        ))
+        list = []
+        for count, row in enumerate(reader):
+            if count == 0:
+                continue
+            list.append(PersonalData(
+            user_id =           row[0],
+            dbnos =             row[1],
+            assists =           row[2],
+            boosts =            row[3],
+            damage_dealt =      row[4],
+            headshot_kills =    row[5],
+            heals =             row[6],
+            kills =             row[7],
+            longest_kill =      row[8],
+            revives =           row[9],
+            total_distance =    row[10],
+            ride_distance =     row[11],
+            road_kills =        row[12],
+            swim_distance =     row[13],
+            vehicle_destroys =  row[14],
+            time_survived =     row[15],
+            walk_distance =     row[16],
+            weapons_acquired =  row[17],
+            team_count =        row[18],
+            map_name =          row[19],
+            duration =          row[20],
+            win_place =         row[21]
+            ))
 
-    PersonalData.objects.bulk_create(list)  # 리스트를 한번에 입력
-    match_data = PersonalData.objects.filter(user_id=name)
-    page = request.GET.get('page', '1')  # 페이지
-    paginator = Paginator(match_data, 10)  # 페이지당 20개 씩 보여주기
-    page_obj = paginator.get_page(page)
-    context = {'PersonalData': page_obj}
-    return render(request, "rank/rank_home.html", context)
+        PersonalData.objects.bulk_create(list)  # 리스트를 한번에 입력
+        match_data = PersonalData.objects.filter(user_id=name)
+        page = request.GET.get('page', '1')  # 페이지
+        paginator = Paginator(match_data, 10)  # 페이지당 20개 씩 보여주기
+        page_obj = paginator.get_page(page)
+        context = {'PersonalData': page_obj}
+        return render(request, "rank/rank_home.html", context)
+    except:
+        print("데이터 크롤링중 오류발생, 아이디 및 플랫폼 확인 필요")
+        return render(request, "rank/rank_home.html")
 
-
-def predict_rank(request, id):
-    match_data = PersonalData.objects.filter(id=id).values()
-    # object = get_object_or_404(match_data)
-
-    print(match_data[0]['win_place'])
-    context = {
-        'id' : match_data[0]['id'],
-        'pre_ranking' : match_data[0]['win_place']
-    }
-    return render(request, 'rank/rank_home.html', context)
-    # return JsonResponse(data)
-    # 같은 홈페이지에 머물기
-    # return redirect(request.META.get("HTTP_REFERER", 'redirect_if_referer_not_found'))
-
-# ajax 코드 작성 필요.. json 형식 전송까지 완료ㅇㄱ쇼ㅕ8
-def likes(request):
-    print("dd")
+# ajax로 순위예측 하기
+def predict_rank(request):
+    import pandas as pd
     if request.META.get("HTTP_X_REQUESTED_WITH") == 'XMLHttpRequest':  # ajax 방식일 때 아래 코드 실행
-        # id = request.GET['id']  # 좋아요를 누른 게시물id (blog_id)가지고 오기
-        # post = PersonalData.objects.get(id=id)
-        # print(post)
-        message = "좋아요 취소"
-        print("asdf")
-        '''user = request.user  # request.user : 현재 로그인한 유저
-        if post.like.filter(id=user.id).exists():  # 이미 좋아요를 누른 유저일 때
-            post.like.remove(user)  # like field에 현재 유저 추가
-            message = "좋아요 취소"  # 화면에 띄울 메세지
-        else:  # 좋아요를 누르지 않은 유저일 때
-            post.like.add(user)  # like field에 현재 유저 삭제
-            message = "좋아요"  # 화면에 띄울 메세지
-        # post.like.count() : 게시물이 받은 좋아요 수'''
-        context = {'like_count': 1, "message": message}
+        # 데이터 불러오기
+        id = request.GET['msg']  # 고유번호 가져오기
+        match_data = PersonalData.objects.filter(id=id).values()
+        result = predict_one(match_data[0])
+        result_msg = str(result) + "위"
+        context = {
+            'id': match_data[0]['id'],
+            'result_show': result_msg
+        }
         return HttpResponse(json.dumps(context), content_type='application/json')
